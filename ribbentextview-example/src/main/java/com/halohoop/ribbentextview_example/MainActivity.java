5f7a5d90db3d3aa2a6e47c278e9814217d6a4839
halohoop <halohoop@163.com>
2016-07-27 09:20:07 +0800

    * update usage demo; * add multiple attrs;

--- ribbentextview-example/src/main/java/com/halohoop/ribbentextview_example/MainActivity.java
+++ ribbentextview-example/src/main/java/com/halohoop/ribbentextview_example/MainActivity.java
@@ -1,13 +1,25 @@
 package com.halohoop.ribbentextview_example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.halohoop.ribbentextview.RibbenTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RibbenTextView rtv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rtv = (RibbenTextView) findViewById(R.id.rtv);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                rtv.setText("Halohoop-Wong");
+            }
+        },2000);
     }
 }
--- ribbentextviewlibrary/src/main/java/com/halohoop/ribbentextview/RibbenTextView.java
+++ ribbentextviewlibrary/src/main/java/com/halohoop/ribbentextview/RibbenTextView.java
@@ -25,7 +25,9 @@ public class RibbenTextView extends View {
     private int mTextColor = android.graphics.Color.parseColor("#FFFFFFFF");//white
     private int mRibbenColor = android.graphics.Color.parseColor("#FFFF0000");//red
     private Rect mTextRect;
+    private TextRectWidhPadding mTextRectWidhPadding;
     private float mTextSize = 12;
+    private int mTextPadding = 2;
 
     public RibbenTextView(Context context) {
         this(context, null);
@@ -44,12 +46,14 @@ public class RibbenTextView extends View {
             throw new RuntimeException("please use attr text to add a text for this widget!");
         }
         int defaultTextSize = DensityUtil.dip2px(context, mTextSize);
+        int defaultTextPadding = DensityUtil.dip2px(context, mTextPadding);
         mTextSize = attributes.getFloat(R.styleable.RibbenTextView_text_size, defaultTextSize);
+        mTextPadding = attributes.getInteger(R.styleable.RibbenTextView_text_padding, defaultTextPadding);
         mTextSize = DensityUtil.dip2px(context, mTextSize);
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mTextPaint.setTextSize(mTextSize);
         mRibbenPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        getTextHeight();//get the width and height of the text;
+        getTextHeight();//get the width and height of the text based on the mTextSize;
         mRotateDegrees = attributes.getFloat(R.styleable.RibbenTextView_rotate_degree, mRotateDegrees);
         mTextColor = attributes.getColor(R.styleable.RibbenTextView_text_color, mTextColor);
         mRibbenColor = attributes.getColor(R.styleable.RibbenTextView_ribben_color, mRibbenColor);
@@ -58,22 +62,27 @@ public class RibbenTextView extends View {
         attributes.recycle();
     }
 
+    class TextRectWidhPadding {
+        int width;
+        int height;
+    }
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         //TODO 根据文字的宽度高度 以及角度 计算出宽高
-        int textWidth = mTextRect.width();
-        int textHeight = mTextRect.height();
+        int textWidthWithPadding = mTextRectWidhPadding.width;
+        int textHeightWithPadding = mTextRectWidhPadding.height;
         //计算出宽度
         double tan = Math.tan(mRotateDegrees * Math.PI / 180);
-        double v = textHeight / tan;
-        double sqrt = Math.sqrt(textHeight * textHeight + v * v);
-        double v1 = textHeight * v / sqrt;
+        double v = textHeightWithPadding / tan;
+        double sqrt = Math.sqrt(textHeightWithPadding * textHeightWithPadding + v * v);
+        double v1 = textHeightWithPadding * v / sqrt;
         double v2 = v1 / tan;
         double sin = Math.sin(mRotateDegrees * Math.PI / 180);
-        double v3 = textWidth * sin;
+        double v3 = textWidthWithPadding * sin;
         double v4 = v3 / tan;
-        double v5 = textHeight * tan;
+        double v5 = textHeightWithPadding * tan;
         double cos = Math.cos(mRotateDegrees * Math.PI / 180);
         double v6 = v5 * cos;
         double v7 = v6 * tan;
@@ -82,21 +91,24 @@ public class RibbenTextView extends View {
         setMeasuredDimension(Math.abs(measuredWidth), Math.abs(measuredHeight));
     }
 
-    private int getHeightCalculateFromMeasuredWidth(int measuredWidth, float mRotateDegrees) {
-        if (mRotateDegrees == 0) {
-            return mTextRect.height() + getPaddingBottom() + getPaddingTop();
-        }
-        double tan = Math.tan(mRotateDegrees * Math.PI / 180);
-        double v = tan * (float) measuredWidth;
-        return Math.abs((int) Math.round(v));
-    }
+//    private int getHeightCalculateFromMeasuredWidth(int measuredWidth, float mRotateDegrees) {
+//        if (mRotateDegrees == 0) {
+//            return mTextRect.height() + getPaddingBottom() + getPaddingTop();
+//        }
+//        double tan = Math.tan(mRotateDegrees * Math.PI / 180);
+//        double v = tan * (float) measuredWidth;
+//        return Math.abs((int) Math.round(v));
+//    }
 
     @Override
     protected void onDraw(Canvas canvas) {
+//        int height = mTextRect.height();
+        int height = mTextRectWidhPadding.height;
         double tan = Math.tan(mRotateDegrees * Math.PI / 180);
-        int transDistance = (int) Math.round(mTextRect.height() / tan);
+        int transDistance = (int) Math.round(height / tan);
         canvas.rotate(mRotateDegrees, 0, 0);
-        canvas.translate(transDistance, 0);
+        //需要把padding值给加上
+        canvas.translate(transDistance, -mTextPadding);//旋转之后就是斜着移动了
         canvas.drawText(mText, 0, 0, mTextPaint);
     }
 
@@ -105,13 +117,22 @@ public class RibbenTextView extends View {
             mTextRect = new Rect();
         }
         mTextPaint.getTextBounds(mText, 0, mText.length(), mTextRect);
-        LogUtils.i("textWeight:" + mTextRect.width());
+        if (mTextRectWidhPadding == null) {
+            mTextRectWidhPadding = new TextRectWidhPadding();
+        }
+        mTextRectWidhPadding.width = mTextRect.width() + mTextPadding * 2;
+        mTextRectWidhPadding.height = mTextRect.height() + mTextPadding * 2;
+        LogUtils.i("textWidth:" + mTextRect.width());
         LogUtils.i("textHeight:" + mTextRect.height());
+        LogUtils.i("textWidthWidhPadding:" + mTextRectWidhPadding.width);
+        LogUtils.i("textHeightWidhPadding:" + mTextRectWidhPadding.height);
     }
 
     public void setText(String text) {
         this.mText = text;
-        //TODO update degree
+        getTextHeight();
+        requestLayout();
+        invalidate();
     }
 
     public String getText() {
